eb1e2cbb3c895a6c1f7b4011bd826f2014bbecb6
Ian Pritchard <pritchardians@gmail.com>
2022-02-06 16:05:38 -0600

    responsive menu styled

---
+++ static/js/components/navbar/navbar.js
@@ -0,0 +1,12 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+console.log("Top Nav code running");
+const navBarResponsive = () => {
+    let navBar = document.getElementById("top-nav");
+    // console.log(navBar);
+    if (navBar.className === "top-nav") {
+        navBar.className += " responsive";
+    }
+    else {
+        navBar.className = "top-nav";
+    }
+};
