050ca5409e784ca27bd23c5288417236bd0894bb
Ian Pritchard <pritchardians@gmail.com>
2022-02-07 16:04:18 -0600

    added buttons for font size

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 console.log("Javascript wired up correctly. Compiled from Typescript");
 // This file should consist only of imports from components
 import { wiredUp } from "../components/whole-site/wholesite.js";
+import { fontSizeSmall } from "../components/whole-site/wholesite.js";
+import { fontSizeMedium } from "../components/whole-site/wholesite.js";
+import { fontSizeLarge } from "../components/whole-site/wholesite.js";
 
 wiredUp();
+fontSizeSmall();
+fontSizeMedium();
+fontSizeLarge();
